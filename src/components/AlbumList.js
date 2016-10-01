949653af6be275da1c9d51aef659361acc23671d
Avinash Singh <avinash4507@gmail.com>
2016-10-02 00:31:32 +0530

    converted functional compo into class based compo as we need more functionalities

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>AlbumsList!!!</Text>
-        </View>
-    );
+class AlbumList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>AlbumsList!!!</Text>
+            </View>
+        );
+    }
 };
 
 export default AlbumList;
