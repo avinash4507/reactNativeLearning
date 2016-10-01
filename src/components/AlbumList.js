dae0c13916a9f13f8d63adbaf69206b73d9f82ac
Avinash Singh <avinash4507@gmail.com>
2016-10-02 01:39:05 +0530

    use compo level state for rerendering data onto screen

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,14 +3,20 @@ import { Text, View } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    state = { albums: [] };
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response));
+            .then(response => this.setState({ albums: response.data }));
+    }
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <Text key={album.title}>{album.title}</Text>
+        );
     }
     render() {
         return (
             <View>
-                <Text>AlbumsList!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
