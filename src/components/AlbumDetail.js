3e2b0e14411048c042bd8e96fae0c518d84dc58b
Avinash Singh <avinash4507@gmail.com>
2016-10-03 23:15:53 +0530

    added button url links

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-    const { thumbnail_image, title, artist, image } = album;
+    const { thumbnail_image, title, artist, image, url } = album;
     const { thumbnailStyle,
         headerContentStyle,
         thumbnailContainerStyle,
@@ -27,7 +27,9 @@ const AlbumDetail = ({ album }) => {
                 <Image style={imageStyle} source={{ uri: image }} />
             </CardSection>
             <CardSection>
-                <Button whenPressed={() => console.log(title)} />
+                <Button whenPressed={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
-const Button = ({ whenPressed }) => {
+const Button = ({ whenPressed, children }) => {
     const { buttonStyle, textStyle } = styles;
     return (
         <TouchableOpacity onPress={whenPressed} style={buttonStyle}>
-            <Text style={textStyle}>Click Me!!!</Text>
+            <Text style={textStyle}>
+                {children}
+            </Text>
         </TouchableOpacity>
     );
 };
