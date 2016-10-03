235396a58ff862de7979f28d68891ff383c8cf0c
Avinash Singh <avinash4507@gmail.com>
2016-10-03 22:55:35 +0530

    added scrolling, button, and various stylings

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -15,10 +15,11 @@ class AlbumList extends Component {
         );
     }
     render() {
+        console.log(this.state);
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
